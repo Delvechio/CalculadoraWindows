cc07dcab08bcb66aeec0bb4171fc40efd0c69ba4
Delvechio Duarte da Silva Júnior <delvechioduarte2502@gmail.com>
2021-06-07 09:18:04 -0300

    Fazendo os números funcionarem

--- CalculadoraWindows/CalculadoraWindows/MainPage.xaml.cs
+++ CalculadoraWindows/CalculadoraWindows/MainPage.xaml.cs
@@ -8,11 +8,56 @@ using Xamarin.Forms;
 
 namespace CalculadoraWindows
 {
+    
     public partial class MainPage : ContentPage
     {
+        int currentState = 1;
+        string mathOperator;
+        double firsNumber, secondNumber;
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            firsNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            this.resultText.Text = "0";
+
+        }
+        void onSelectNumber(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            this.resultText.Text = "0";
+
+            if (this.resultText.Text == "0" || currentState < 0)
+            {
+                this.resultText.Text = "";
+                if(currentState < 0)
+                {
+                    currentState *= -1;
+                }
+                this.resultText.Text += pressed;
+                double number;
+                if (double.TryParse(this.resultText.Text, out number))
+                {
+                    this.resultText.Text = number.ToString("NO");
+                    if (currentState == 1)
+                    {
+                        firsNumber = number;
+                    }
+                    else
+                    {
+                        secondNumber = number;
+                    }
+                }
+            } 
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            OnClear(new object(), new EventArgs());
         }
+
     }
 }
